6111d129711e88c0e7712b24ae634be08009b097
maiha <maiha.219@gmail.com>
2024-10-12 00:32:35 +1000

    update version

--- version.php
+++ version.php
@@ -2,6 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024092500;
-$plugin->requires  = 2024092500;
+$plugin->version   = 2024101200;
+$plugin->requires  = 2024041600;
 $plugin->component = 'tiny_teacheraide';
