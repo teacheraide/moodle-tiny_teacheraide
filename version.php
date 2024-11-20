6256310901147cae45a4f7afc25e8247fd3b5875
Freeman Man <freeman@ycfreeman.com>
2024-11-20 21:20:28 +1000

    bump version

--- version.php
+++ version.php
@@ -2,12 +2,14 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024101201;
+
+$plugin->release = '0.2.0';
+$plugin->version   = 2024112001;
 $plugin->requires  = 2024041600;
 $plugin->component = 'tiny_teacheraide';
 
 $plugin->dependencies = [
-  'local_teacheraide' => 2024091002,
+  'local_teacheraide' => 2024112001,
 ];
 
 $plugin->maturity = MATURITY_ALPHA;
